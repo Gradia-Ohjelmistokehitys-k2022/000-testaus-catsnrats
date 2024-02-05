41eb7f12ec1358662754b10b447b7fc6b2008c4d
Antti <antti.se@gmail.com>
2024-02-05 17:15:15 +0200

    pankkitestien pohja

---
+++ 01_BasicExcercises/BankTests/BankAccountTests.cs
@@ -0,0 +1,13 @@
+using BankAccountNS;
+
+namespace BankTests
+{
+    [TestClass]
+    public class BankAccountTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
